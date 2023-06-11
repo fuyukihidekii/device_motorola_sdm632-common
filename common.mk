1bebe077e1a99a65b017da2d53ad07c2f1ab111e
Eduardo 'Hideki' S. M <fuyukihidekii@gmail.com>
2023-06-11 00:44:17 -0300

    build aosp camera2

--- common.mk
+++ common.mk
@@ -114,7 +114,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5 \
     camera.device@3.2-impl:32 \
     libbson.vendor \
-    libxml2
+    libxml2 \
+    Camera2
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
